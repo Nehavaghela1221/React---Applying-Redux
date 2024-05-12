5c458d49849010245bc41662ef9391056f4e2667
Neha Vaghela <136678351+Nehavaghela1221@users.noreply.github.com>
2024-05-12 10:06:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { createContext, useState , useContext } from 'react';
+import LoginForm from './LoginForm';
+import styled from 'styled-components';
+const ThemeContext = createContext({
+    theme: 'light',
+    toggleTheme: () => {},
+});
+
+
+const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+   
+    const contextValue = {
+        theme,
+        toggleTheme,
+    };
+
+    return (
+        <ThemeContext.Provider value={contextValue}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+const MyButton = styled.button`
+  display: inline-block;
+  padding: 0.7rem 1.2rem;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  color: #fff;
+  background-color: #af781b;
+  
+`;
+const MyComponent = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
+    return (
+        <div style={{ backgroundColor: theme === 'light' ? 'white' : 'black' , color: theme === 'light' ? 'black' : 'white'  }} > <center>
+          <MyButton  onClick={toggleTheme}>Dark/Light</MyButton></center>
+            <LoginForm/>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <ThemeProvider>
+            <MyComponent />
+        </ThemeProvider>
+    );
+};
 
 export default App;
